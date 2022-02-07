f3b017faeaf03914e93b71b63f0acfbb8950ea2c
mattecolo <teocolombo03@gmail.com>
2022-02-07 15:47:46 +0100

    cs pagella

---
+++ client_pagella.py
@@ -0,0 +1,32 @@
+import socket
+import json
+
+HOST="127.0.0.1"
+PORT=65432
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    #AF_INEF: tipo indirizzo/protocollo usato; la famiglia dell'indirizzo
+    #STREAM: TCP. Indica la connessione, per l'appunto
+    s.connect((HOST, PORT))
+    while True:
+        operazione=input("#list : per vedere i voti inseriti /n #set /nomestudente : per inserire uno studente /n #put /nomestudente/materia/voto/ore /n #get /nomestudente : per richiedere i voti di uno studente /n #exit : per chiudere la connessione con il server /n #close: per chiudere la connessione con il server e per spegnerlo")
+        messaggio={
+            'operazione' : operazione
+        }
+        messaggio=json.dumps(messaggio)#Trasforma l'oggetto in stringa
+        s.sendall(messaggio.encode("UTF-8"))
+        #UTF-8 è la famiglia dei caratteri, utilizzato anche in HTML
+        data=s.recv(1024)
+        if operazione=="#list":
+            print(data.decode())
+            deserialized_dict=json.loads(data)#decodifica dopo aver ricevuto
+        elif data.find('#set') != -1:
+            deserialized_dict=json.loads(data)#decodifica dopo aver ricevuto
+        elif operazione=="#close":
+            print("Connessione chisa")
+            break
+        else:
+            deserialized_dict(data.decode())
+        print("\n")
+        print(deserialized_dict,"\n")
+        #Fine parte client
