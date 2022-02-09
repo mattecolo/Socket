910a63e4179b32db3e04307cc8efc3f3c6046edb
mattecolo <teocolombo03@gmail.com>
2022-02-09 18:14:25 +0100

    calco2

---
+++ calcoserver2.0.py
@@ -0,0 +1,59 @@
+import socket
+from threading import Thread
+import json
+
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 22224
+
+def ricevi_comandi(sock_service,addr_client):#codice programma calcolatrice precedente
+    print("avviato")
+    while True:
+        data=sock_service.recv(1024)
+        if not data: #se data è un vettore vuoto risulta false; sennò true/if len(data)==0/se è vuoto esce, sennò continua
+                break
+        data=data.decode()
+        data=json.loads(data)
+        primoNumero=data['primoNumero']
+        operazione=data['operazione']
+        secondoNumero=data['secondoNumero']
+        ris=""
+        if operazione=="+":
+            ris=primoNumero+secondoNumero
+        elif operazione=="-":
+            ris=primoNumero-secondoNumero
+        elif operazione=="*":
+            ris=primoNumero*secondoNumero
+        elif operazione=="/":
+            if secondoNumero==0:
+                ris="Non puoi dividere per 0"
+            else:
+                ris=primoNumero/secondoNumero
+        elif operazione=="%":
+            ris=primoNumero%secondoNumero
+        else:
+            ris="Operazione non riuscita"
+        ris=str(ris)#Casting a stringa
+        sock_service.sendall(ris.encode("UTF-8"))
+    sock_service.close()
+
+def ricevi_connessioni(sock_listen):#funzione che accetta la connessione del client, al suo interno crea dei thread per eseguire le richieste che arrivano dal client
+    while True:
+        sock_service, addr_client = sock_listen.accept()
+        print("\nConnessione ricevuta da " + str(addr_client))
+        print("\nCreo un thread per servire le richieste ")
+        try:
+            Thread(target=ricevi_comandi,args=(sock_service,addr_client)).start()
+        except:
+            print("il thread non si avvia")
+            sock_listen.close()
+        
+def avvia_server(indirizzo,porta):#funzione che avvia il server
+    sock_listen=socket.socket()
+    sock_listen.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    sock_listen.bind((SERVER_ADDRESS,SERVER_PORT))
+    sock_listen.listen(5)
+    print("Server in ascolto su %s." %str((SERVER_ADDRESS,SERVER_PORT)))
+    ricevi_connessioni(sock_listen)
+
+if __name__=='__main__':
+    avvia_server(SERVER_ADDRESS,SERVER_PORT)
