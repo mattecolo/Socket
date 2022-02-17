bf3e095d9de585a7755e65063791d10d18f107e7
mattecolo <teocolombo03@gmail.com>
2022-02-17 10:03:10 +0100

    ok

--- calcoserver2.0.py
+++ calcoserver2.0.py
@@ -5,7 +5,7 @@ import json
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22001
 
-def ricevi_comandi(sock_service):#codice programma calcolatrice precedente
+def ricevi_comandi(sock_service,addr_client):#codice programma calcolatrice precedente
     print("avviato")
     while True:
         data=sock_service.recv(1024)
@@ -36,7 +36,7 @@ def ricevi_comandi(sock_service):#codice programma calcolatrice precedente
         sock_service.sendall(ris.encode("UTF-8"))
     sock_service.close()
 
-def ricevi_connessioni(sock_listen,addr_client):#funzione che accetta la connessione del client, al suo interno crea dei thread per eseguire le richieste che arrivano dal client
+def ricevi_connessioni(sock_listen):#funzione che accetta la connessione del client, al suo interno crea dei thread per eseguire le richieste che arrivano dal client
     while True:
         sock_service, addr_client = sock_listen.accept()
         print("\nConnessione ricevuta da " + str(addr_client))
