b592091210800d31738350dcbef6fbb70d6ee83a
mattecolo <teocolombo03@gmail.com>
2022-03-03 12:28:54 +0100

    commenti

--- calcoclientmultithreading.py
+++ calcoclientmultithreading.py
@@ -23,10 +23,10 @@ def genera_richieste(num,address,port):
         print(f"{threading.current_thread().name} Qualcosa è andato storto, sto uscendo... \n")
         sys.exit()
     #1. rimpiazzare questa parte con la generazione di operazioni e numeri random, non vogliamo inviare sempre 3+5 
-    primoNumero=random.randint(0,100)
+    primoNumero=random.randint(0,100)#assegno ai due numeri un valore casuale tra 0 e 100
     secondoNumero=random.randint(0,100)
-    listaoperazioni=["+","-","*","/","%"]
-    operazione=listaoperazioni[random.randint(0,4)]
+    listaoperazioni=["+","-","*","/","%"]#lista con le operazioni
+    operazione=listaoperazioni[random.randint(0,4)]#pesca un operazione dalla lista
     '''
     Soluzione alternativa
     Numoperazione=random.randint(0,4)
@@ -43,54 +43,52 @@ def genera_richieste(num,address,port):
     #print(primoNumero)
     #print(secondoNumero)
     #print(operazione)
-    sock_service=socket.socket()
-    sock_service.connect((address,port))
-    messaggio={'primoNumero':primoNumero, 'operazione':operazione, 'secondoNumero':secondoNumero}
+    messaggio={'primoNumero':primoNumero, 'operazione':operazione, 'secondoNumero':secondoNumero}#mostra i due numeri e l'operazione
     messaggio=json.dumps(messaggio)#Trasforma l'oggetto in stringa
-    print("Invio richesta ",messaggio)
-    sock_service.sendall(messaggio.encode("UTF-8"))
-    data=sock_service.recv(1024)
+    print("Invio richesta ",messaggio)#mostra l'operazione
+    s.sendall(messaggio.encode("UTF-8"))
+    data=s.recv(1024)
     
-    if not data:
+    if not data:#se il server non manda una risposta
         print(f"{threading.current_thread().name}: Server non risponde. Exit")
-    else:
+    else:#se il server manda una risposta
         print(f"{threading.current_thread().name}: Risultato: {data.decode()}") # trasforma il vettore di byte in stringa
     s.close()
     end_time_thread=time.time()
     print(f"{threading.current_thread().name} tempo di esecuzione time=", end_time_thread-start_time_thread)
 
 if __name__ == '__main__':
-    start_time=time.time()
+    start_time=time.time()#prende il tempo all'inizio dell'esecuzione
     # 3 ciclo per chiamare NUM_WORKERS volte la funzione genera richieste alla quale passo i parametri (num,SERVER_ADDRESS, SERVER_PORT)
     for cont in range(NUM_WORKERS):
-        genera_richieste(cont,SERVER_ADDRESS,SERVER_PORT)
-    end_time=time.time()
-    print("Total SERIAL time=", end_time - start_time)
+        genera_richieste(cont,SERVER_ADDRESS,SERVER_PORT)#chiamata alla funzione
+    end_time=time.time()#prende il tempo all'inizio dell'esecuzione
+    print("Total SERIAL time=", end_time - start_time)#mostra il tempo di esecuzione facendo la sottrazione tra il tempo finale e iniziale
      
-    start_time=time.time()
-    threads=[]
+    start_time=time.time()#prende il tempo all'inizio dell'esecuzione
+    threads=[]#lista dei thread
     # 4 ciclo per chiamare NUM_WORKERS volte la funzione genera richieste tramite l'avvio di un thread al quale passo i parametri args=(num,SERVER_ADDRESS, SERVER_PORT,)
-    for num in range(NUM_WORKERS):
-        thread=threading.Thread(target=genera_richieste ,args=(num,SERVER_ADDRESS, SERVER_PORT))
+    for num in range(NUM_WORKERS):#per ogni "lavoratore"
+        thread=threading.Thread(target=genera_richieste ,args=(num,SERVER_ADDRESS, SERVER_PORT))#creazione dei thread con target e parametri
     # ad ogni iterazione appendo il thread creato alla lista threads
-        threads.append(thread)
+        threads.append(thread)#lo aggiungo alla lista
     # 5 avvio tutti i thread
-    [thread.start() for thread in threads]
+    [thread.start() for thread in threads]#partono tutti i thread
     # 6 aspetto la fine di tutti i thread 
-    [thread.join() for thread in threads]
-    end_time=time.time()
-    print("Total THREADS time= ", end_time - start_time)
+    [thread.join() for thread in threads]#con join il programma attende la fine dei thread prima di terminare 
+    end_time=time.time()#prende il tempo alla fine dell'esecuzione
+    print("Total THREADS time= ", end_time - start_time)#mostra il tempo di esecuzione facendo la sottrazione tra il tempo finale e iniziale
 
-    start_time=time.time()
-    process=[]
+    start_time=time.time()#prende il tempo all'inizio dell'esecuzione
+    process=[]#lista dei processi
     # 7 ciclo per chiamare NUM_WORKERS volte la funzione genera richieste tramite l'avvio di un processo al quale passo i parametri args=(num,SERVER_ADDRESS, SERVER_PORT,)
     for num in range(NUM_WORKERS):
-        processo=multiprocessing.Process(target=genera_richieste , args=(num,SERVER_ADDRESS, SERVER_PORT))
-    # ad ogni iterazione appendo il thread creato alla lista threads
-        process.append(processo)
+        processo=multiprocessing.Process(target=genera_richieste , args=(num,SERVER_ADDRESS, SERVER_PORT))#creazione dei processi con target e parametri
+    # ad ogni iterazione appendo il processo creato alla lista process
+        process.append(processo)#lo aggiungo alla lista
     # 8 avvio tutti i processi
-    [processo.start() for processo in process]
+    [processo.start() for processo in process]#partono tutti i processi
     # 9 aspetto la fine di tutti i processi 
-    [processo.join() for processo in process]
-    end_time=time.time()
-    print("Total PROCESS time= ", end_time - start_time)
+    [processo.join() for processo in process]#con join il programma attende la fine dei processi prima di terminare 
+    end_time=time.time()#prende il tempo alla fine dell'esecuzione
+    print("Total PROCESS time= ", end_time - start_time)#mostra il tempo di esecuzione facendo la sottrazione tra il tempo finale e iniziale
